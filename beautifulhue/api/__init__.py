10cb4bd983748b22bc9974ff677fd5455131717e
schaef <martinschaef@gmail.com>
2015-03-21 22:14:23 -0700

    include scene in api

--- beautifulhue/api/__init__.py
+++ beautifulhue/api/__init__.py
@@ -6,12 +6,14 @@ class Bridge():
         from _config import Config
         from _group import Group
         from _light import Light
+        from _scene import Scene
         from _schedule import Schedule
     
         self.config = Config(device, user, debug)
         self.group = Group(device, user, debug)
         self.light = Light(device, user, debug)
         self.schedule = Schedule(device, user, debug)
+        self.scene = Scene(device, user, debug)
         
 class Portal(Portal):
     
